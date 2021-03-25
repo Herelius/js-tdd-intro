5e82adbb21a1d1533525bf5040eed6a79bed4b80
Youcef Boutrig <youcefboutrig92@gmail.com>
2021-03-25 12:08:41 +0100

    update format for eslint

---
+++ information.js
@@ -0,0 +1,9 @@
+const name = 'John Doe';
+const age = 26;
+const passions = ['Ski', 'Hiking', 'Cinema'];
+
+const myInfos = () => {
+    console.log(`Hi my name is ${name}, I'm ${age} and my passions are ${passions}`);
+};
+
+module.exports = myInfos;
